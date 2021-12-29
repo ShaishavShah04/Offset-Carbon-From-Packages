4074d131d6cc22333bed8d21555fe6e443000433
Shaishav Shah <shaishav.shah04@gmail.com>
2021-12-28 17:40:59 -0700

    Added the "Result" model.

---
+++ Models/ResultModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarbonOffset.Models
+{
+    public class Result
+    {
+        // Properties
+        public int Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string StartingCity { get; set; }
+        public string DestinationCity { get; set; }
+        public int Distance { get; set; }
+        public int Cost { get; set; }
+    }
+}
