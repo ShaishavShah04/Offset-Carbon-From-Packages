e24989888eac97b445e8178912f07dc7233439f6
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2021-12-29 14:04:19 -0700

    http get request completed. Working on parsing response

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using CarbonOffset.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace CarbonOffset.Controllers
 {
@@ -29,16 +32,49 @@ namespace CarbonOffset.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        //static HttpClient client = new HttpClient();
+
+        // private IHttpClientFactory _httpClientFactory;
         [HttpPost]
-        public async Task<IActionResult> Result(string trackingNumber, string carrier)
+        public async Task<IActionResult> Result(Result resultObject)
         {
-            Console.WriteLine(carrier);
-            /*
-            using (var client = new HttpClient())
-            { 
-                var uri = new Uri("https://api.goshippo.com/tracks/shippo/SHIPPO_TRANSIT")
+            string carrier = resultObject.Carrier;
+            string trackingNumber = resultObject.TrackingNumber;
+            string path = $"https://api.goshippo.com/tracks/{carrier}/{trackingNumber}";
+            Console.WriteLine(resultObject.Carrier);
+            Console.WriteLine(resultObject.TrackingNumber);
+
+            // testing purposes URL
+            // https://api.goshippo.com/tracks/shippo/SHIPPO_TRANSIT
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(path);
+
+            client.DefaultRequestHeaders.Add("Authorization", "ShippoToken shippo_test_ac3dd19acaa600c13da4b0bde6220d3da0c690b0");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            HttpResponseMessage response = client.GetAsync("").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                using (HttpContent content = response.Content) 
+                {
+                    Task<string> result = content.ReadAsStringAsync();
+                    var trackingDetails = JsonConvert.DeserializeObject(result.Result);
+                    Console.WriteLine(trackingDetails);
+                    
+                    Result trackingObject = new Result();
+                    trackingObject.Carrier = carrier;
+                    trackingObject.TrackingNumber = trackingNumber;
+                    
+                }
             }
-            */
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            client.Dispose();
+
             return RedirectToAction("Index", "Result");
         }
     }
