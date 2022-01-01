6f1be98613ef09771e4e644dc06d9d85642387a5
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2021-12-31 17:54:55 -0700

    http request with shippo working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,18 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Diagnostics;
-using Newtonsoft.Json;
+using Microsoft.Net.Http.Headers;
+using System.Text.Json;
+
 
 namespace CarbonOffset.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly IHttpClientFactory _httpClientFactory;
+        public HomeController(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
 
         public IActionResult Index()
         {
@@ -32,9 +30,6 @@ namespace CarbonOffset.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        //static HttpClient client = new HttpClient();
-
-        // private IHttpClientFactory _httpClientFactory;
         [HttpPost]
         public async Task<IActionResult> Result(Result resultObject)
         {
@@ -47,35 +42,58 @@ namespace CarbonOffset.Controllers
             // testing purposes URL
             // https://api.goshippo.com/tracks/shippo/SHIPPO_TRANSIT
 
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(path);
-
-            client.DefaultRequestHeaders.Add("Authorization", "ShippoToken shippo_test_ac3dd19acaa600c13da4b0bde6220d3da0c690b0");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            
-            HttpResponseMessage response = client.GetAsync("").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
-            if (response.IsSuccessStatusCode)
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, path)
             {
-                using (HttpContent content = response.Content) 
+                Headers =
                 {
-                    Task<string> result = content.ReadAsStringAsync();
-                    var trackingDetails = JsonConvert.DeserializeObject(result.Result);
-                    Console.WriteLine(trackingDetails);
-                    
-                    Result trackingObject = new Result();
+                    { HeaderNames.Authorization, "ShippoToken shippo_test_ac3dd19acaa600c13da4b0bde6220d3da0c690b0" }
+                }
+            };
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            Result trackingObject = new Result();
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                using (HttpContent content = httpResponseMessage.Content)
+                {
+
+                    RawTracking rawDetails = await content.ReadFromJsonAsync<RawTracking>();
+
                     trackingObject.Carrier = carrier;
                     trackingObject.TrackingNumber = trackingNumber;
-                    
+                    trackingObject.StartingCity = rawDetails.address_from.city;
+                    trackingObject.StartCountry = rawDetails.address_from.country;
+                    trackingObject.DestinationCity = rawDetails.address_to.city;
+                    trackingObject.DestinationCountry = rawDetails.address_to.country;
+
                 }
             }
             else
             {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine("{0} ({1})", (int)httpResponseMessage.StatusCode, httpResponseMessage.ReasonPhrase);
             }
+            // not sure if this works
+            return RedirectToAction("Index", "Result", new { result = trackingObject });
+        }
 
-            client.Dispose();
+        public class locationInfo
+        { 
+            public string city { get; set; }
+            public string country { get; set; }
 
-            return RedirectToAction("Index", "Result");
+        }
+
+        public class RawTracking
+        {
+            public locationInfo? address_from { get; set; }
+            public locationInfo? address_to { get; set; }
+
+            public locationInfo GetOriginInfo()
+            {
+                return address_from;
+            }
         }
     }
 }
